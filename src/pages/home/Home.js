db03b146e741e1acb4e859daa5fa4a2bcbe30d71
AhmedE2021 <89975214+AhmedE2021@users.noreply.github.com>
2022-10-20 11:46:01 +0200

    The useNavigate hook on click function that lets you navigate to recipe search page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,37 +1,48 @@
 
 
 import {FaSearch } from "react-icons/fa"
+import { useNavigate } from "react-router-dom";
 import NavBar from "../../components/NavBar"
 import "../../styles/Home.css"
+
+
+
 export default function Home () {
+
+  const navigate = useNavigate();
+  function handleClick(){
+    navigate("/search-recipe")
+  }
 
 return(
 
 <div>
-  <NavBar/>
+  <NavBar/> 
 <section>
 <div className="intro-img">
-<div className="intro-search-box">
-<h1>Whats's Cookin'</h1>
-     <div className="search-form">
+  <div className="intro-search-box">
+    <h1>Whats's Cookin'</h1>
+      <div className="search-form">
 
-     <form className="search">
-     <button className="search__button">
-                    <FaSearch/>
-                </button>
-                <input
+             <form className="search">
+                <button onClick={handleClick} className="search__button">
+                    <FaSearch color="var(-- light-gret)"/>
+                 </button>
+                   <input
                     className="search__input"
-                    placeholder="Recipe/Ingredients"
+                    placeholder="Recipe / Ingredients"
                     type="text"
                     id="search"
-                    
-                />
+                    onClick={handleClick}
+                    />
               
             </form>
       </div>
 </div>
 </div>
-
+</section>
+<section className="recom">
+<h2>Recommended</h2>
 
 </section>
   
