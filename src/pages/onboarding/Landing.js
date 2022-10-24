a9b892cea6fec6a77399ad81c5a3456e0a9dcbc2
sudo-apt-get-good <89986144+toldeveloper@users.noreply.github.com>
2022-10-24 13:27:29 +0200

    work from school

--- src/pages/onboarding/Landing.js
+++ src/pages/onboarding/Landing.js
@@ -1,4 +1,9 @@
 import { useNavigate } from "react-router-dom";
+import "../../styles/onboarding.css"
+import "../../App.css"
+import LogoFrontpage from "../../assets/images/logo512.png"
+import CreateUserBTN from "../../components/CreateUserBTN";
+
 
 export default function Landing() {
 //Ahmed//The useNavigate hook returns a on click function that lets you navigate to next page 
@@ -9,9 +14,29 @@ export default function Landing() {
   
 return (
     <div>
-     
-      <h1>The landing Page</h1>
-        <button onClick={handleClick}>go to preferences page</button>
+
+      <div className="logoContainer">
+        <img className="logo" alt="test" src={LogoFrontpage} />
+      </div>
+
+      <div class="frontpageText">
+        <h1 className="header">What's Cookin'</h1>
+        
+        <h3>We all need to eat, but doing so shouldnt have to be expensive.
+        Utilizing your preferences and the ingredients you have available, we suggest recipes and show, what they will cost to make.</h3>
+      </div>
+        
+      <div class="createButtons">
+        <CreateUserBTN link={handleClick} account="Login with Google"/>
+        <CreateUserBTN link={handleClick} account="Login with Facebook"/>
+        <CreateUserBTN link={handleClick} account="Cookin Account"/>
+      </div>
+
+      <button onClick={handleClick} className="createAccount">
+        Or create an account
+      </button>
+        
+      {/* <button onClick={handleClick}>go to preferences page</button> */}
     </div>
 )
 
--- src/pages/onboarding/Landing2.js
+++ src/pages/onboarding/Landing2.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import PreferenceCard from "../../components/PreferenceCard";
 
 export default function Landing2() {
 
@@ -8,10 +9,26 @@ export default function Landing2() {
   }
  
 return (
-    <div>
-      <h1>Preferences Page</h1>
-      <button onClick={handleClick}>go to landing 3</button>
-    </div>
+  <div className="overflowHidden">
+
+  <div className="test">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#0099ff" fill-opacity="1" d="M0,128L80,154.7C160,181,320,235,480,250.7C640,267,800,245,960,245.3C1120,245,1280,267,1360,277.3L1440,288L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
+</svg>
+  </div>
+    
+  <div className="backgroundShape">njknjknjk</div>
+
+  <div class="cardFlexContainer">
+    <PreferenceCard name="Pasta"/>
+    <PreferenceCard name="Meat"/>
+    <PreferenceCard name="Easy"/>
+    <PreferenceCard name="Vegan"/>
+    <PreferenceCard name="Soup"/>
+    <PreferenceCard name="Deserts"/>
+  </div>
+
+  </div>
 )
 
 
