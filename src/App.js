6d086867707e6eb801a017cb6cf16f1449e0944b
AhmedE2021 <89975214+AhmedE2021@users.noreply.github.com>
2022-10-24 12:39:05 +0200

    update

--- src/App.js
+++ src/App.js
@@ -11,19 +11,13 @@ import Landing5 from "./pages/onboarding/Landing5";
 import Landing6 from "./pages/onboarding/Landing6";
 import List from "./pages/list/List";
 import Discount from "./pages/discount/Discount";
-<<<<<<< HEAD
 import Profile from "./pages/profile/Profile";
 import InRecipe from "./pages/InRecipe/InRecipe";
-=======
-import Profile from "./pages/profile/Profile"
-import SearchRecipe from './pages/home/SearchRecipe';
-
->>>>>>> 59f5c9a5fdc6786b4aab4fe6adcb032936f22765
+import SearchRecipe from "./pages/home/SearchRecipe";
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <Routes>
         <Route path="/whats-cookin/" element={<Landing />} />
         <Route path="/landing2" element={<Landing2 />} />
@@ -32,28 +26,13 @@ function App() {
         <Route path="/landing5" element={<Landing5 />} />
         <Route path="/landing6" element={<Landing6 />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/search-recipe" element={<SearchRecipe/>}/>
         <Route path="/list" element={<List />} />
         <Route path="/discount" element={<Discount />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/InRecipe" element={<InRecipe />} />
         <Route path="/home" element={<Navigate to="/" />} />
       </Routes>
-=======
-     <Routes>
-     <Route path="/whats-cookin/" element={<Landing />} />
-     <Route path="/landing2" element={<Landing2 />} />
-     <Route path="/landing3" element={<Landing3 />} />
-     <Route path="/landing4" element={<Landing4 />} />
-     <Route path="/landing5" element={<Landing5 />} />
-     <Route path="/landing6" element={<Landing6 />} />
-     <Route path="/home" element={<Home />} />
-     <Route path="/search-recipe" element={<SearchRecipe />} />
-     <Route path="/list" element={<List />} />
-     <Route path="/discount" element={<Discount />} />
-     <Route path="/profile" element={<Profile />} />
-     <Route path="/home" element={<Navigate to="/" />} />
-     </Routes>
->>>>>>> 59f5c9a5fdc6786b4aab4fe6adcb032936f22765
     </div>
   );
 }
