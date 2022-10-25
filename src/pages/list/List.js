bc1a2102e198c6cbb43f0b80bf4eb2cdc3ea1fc8
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2022-10-25 19:45:57 +0100

    I trie to work with the Tab component and it seems to be working

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,15 +1,22 @@
 import NavBar from "../../components/NavBar";
 import SLNavBar from "./components/SLnavBar";
-import { Routes, Route } from "react-router-dom";
-import ShoppingListPage from "./Pages/ShoppingList";
-import StockList from "./Pages/StockList";
-import { BrowserRouter } from "react-router-dom";
+import { Tab, Tabs } from "@mui/material";
 import List2 from "./List2";
+import { useState } from "react";
 
 export default function List() {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const handleTab = (event, newValue) => {
+    setCurrentTab(newValue);
+  };
   return (
     <>
       <div>
+        <Tabs value={currentTab} onClick={handleTab}>
+          <Tab label="Tab 1"></Tab>
+          <Tab label="Tab 2"></Tab>
+        </Tabs>
         <h1> List Page</h1>
         <SLNavBar />
         <List2 />
