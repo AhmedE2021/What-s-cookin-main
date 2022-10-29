4d6e2de181ecef039863aa5ef8fd182693cc0d0e
toldeveloper <89986144+toldeveloper@users.noreply.github.com>
2022-10-29 12:12:46 +0200

    Onboarding Components Added

---
+++ src/components/CreateUserBTN.js
@@ -0,0 +1,7 @@
+export default function CreateUserBTN(props) {
+    return (
+        <button onClick={props.link} className="CreateUserBTN">
+            <h3>{props.account}</h3>
+        </button>
+    )
+}
---
+++ src/components/PreferenceCard.js
@@ -0,0 +1,7 @@
+export default function PreferenceCard(props) {
+    return (
+        <div className="preferenceCard">
+            <h3>{props.name}</h3>
+        </div>
+    )
+}
