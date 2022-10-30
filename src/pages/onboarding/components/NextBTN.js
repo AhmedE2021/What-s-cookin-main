be9b0009d8a4ad8bf6dfca16cf69305a8d8dbf53
toldeveloper <89986144+toldeveloper@users.noreply.github.com>
2022-10-30 11:38:52 +0100

    Pages with CSS working again

--- src/pages/onboarding/components/NextBTN.js
+++ src/pages/onboarding/components/NextBTN.js
@@ -1,7 +1,7 @@
 export default function NextBTN(props) {
     return (
         <button onClick={props.link} className="NextBTN">
-            <h3>Next</h3>
+            Next
         </button>
     )
 }
