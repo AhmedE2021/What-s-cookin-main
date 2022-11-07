9cb0eadbdf2412216fc36ab2d8adcc1344564e35
Kaloyan Pepelyashki <93147724+kaloyanpepelyashki@users.noreply.github.com>
2022-11-07 11:51:11 +0000

    Update InRecipe.js

--- src/pages/InRecipe/InRecipe.js
+++ src/pages/InRecipe/InRecipe.js
@@ -1,19 +1,23 @@
+////KALOYAN///
+
+
 import DescriptComponentInRecipe from "./ComponentsInRecipe/inRecipeDescript";
 import TopComponentInRecipe from "./ComponentsInRecipe/InrRecipeTop";
 import TabsComponent from "./ComponentsInRecipe/Tabs/TabsComponent";
-import NavBar from "../../components/NavBar"
+import NavBar from "../../components/NavBar";
 import { useLocation } from "react-router-dom";
+import GoBackNav from "../home/components/SmallComponents/goBakckNavBar";
 
 export default function InRecipe() {
   const location = useLocation();
-  console.log(location.state.instructions);
   return (
     <main className="in-recipe-main">
-      <NavBar/>
+      <GoBackNav />
+      <NavBar />
       <TopComponentInRecipe />
       <DescriptComponentInRecipe />
       <TabsComponent />
-      <br/>  <br/>  <br/>  
+      <br /> <br /> <br />
     </main>
   );
 }
